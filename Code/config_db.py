8b933354c0a03e89248e36e91d4d228d45424b37
AJPB4133 <andrea.jeanpaul99@gmail.com>
2025-03-22 17:47:25 +0100

    create connection db and strassen.json

---
+++ Code/config_db.py
@@ -0,0 +1,9 @@
+'''Dieser Code stellt die die Verbindung mit der Postgres-Datenbank her'''
+
+db_config = {
+    'host': 'localhost', #Hostname von der DB
+    'port': '5432', #Port von der  DB
+    'database': 'IP3-GIS CDE', #Name von der DB
+    'user': 'postgres', #Username für die Verbindung zur DB
+    'password': 'TeamLH44'  #Passwort für Username
+}
