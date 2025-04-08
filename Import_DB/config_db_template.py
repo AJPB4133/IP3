142ddcfa562d400ea85d618ff0f9c638beda1935
AJPB4133 <andrea.jeanpaul99@gmail.com>
2025-04-08 21:38:50 +0200

    Create config_db_template.py

---
+++ Import_DB/config_db_template.py
@@ -0,0 +1,9 @@
+'''Dieser Code stellt die die Verbindung mit der Postgres-Datenbank her'''
+
+db_config = {
+    'host': 'hostname', #Hostname von der DB
+    'port': 'Port', #Port von der  DB
+    'database': 'Name', #Name von der DB
+    'user': 'User', #Username für die Verbindung zur DB
+    'password': 'Passwort'  #Passwort für Username
+}
