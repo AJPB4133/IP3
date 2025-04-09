fd45dedda5c7c57e0d19015adceeb11b200b1c9a
AJPB4133 <andrea.jeanpaul99@gmail.com>
2025-04-09 17:24:33 +0200

    Update Karte.js

--- dummy/src/Karte.js
+++ dummy/src/Karte.js
@@ -13,3 +13,56 @@ import theme from "./theme";
 import { ZoomToExtent, defaults as defaultControls } from "ol/control.js";
 import { createVectorSource } from "./kartenWFS.js";
 import { SwisstopoLayer } from "./swisstopoLayer.js";
+
+const Karte = () => {
+    const mapRef = useRef(null);
+    const [setSelectedFeature, setSelectedFeature] = useState(null);
+
+
+    useEffect (() => {
+    // WFS Anfrage für alle Strassensegmente aus KartenWFS.js
+
+        const strassensegmenteSource = createVectorSource ("strassensegment", bboxStrategy);
+
+
+        // Strassensegmenlayer Style aus kartenlayerstyle.js
+        const strassensegmentLayer = new VectorLayer({
+            source: strassensegmenteSource,
+            style: strassensegmentLayerStyle,
+        });
+
+
+        //Definition des Kartenextents für WMS/WMTS
+        const extent = [2420000, 130000, 2900000, 1350000];
+        // WMS Winterlandeskarte holen mit der Funktion SwisstopoLayer aus dem File swisstopoLayer.js
+        const WMSswissimageLayer = SwisstopoLayer(extent);
+
+        // Layer Reihenfolge festlegen, 0 ist zuunterst
+        WMSswissimageLayer.setZIndex(0);
+        strassensegmentLayer.setZIndex(1);
+
+        // Karte erstellen
+        const map = new Map({
+            //Zoom to Extent Button hinzufügen
+            controls: defaultControls().extend([
+                new ZoomToExtent({
+                    extent: [2755375, 1164628, 2775625, 1195443],
+                }),
+             ]),
+            layers: [
+                WMSswissimageLayer,
+                strassensegmentLayer
+            ],
+            target: mapRef.current,
+            view: new View({
+                center: [2762640.8, 1179359.1],
+                zoom: 12,
+                projection: new Projection({
+                code: "EPSG:2056",
+                units: "m",
+            }),
+        }),
+
+    });
+    }, []);
+}
