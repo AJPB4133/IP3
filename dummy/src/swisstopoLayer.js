2e3e7eed36590886649ab831fb6b6ce5f15f52e6
AJPB4133 <andrea.jeanpaul99@gmail.com>
2025-04-15 21:46:10 +0200

    mape_test.js deleted and Layer.js kommentiert

--- dummy/src/swisstopoLayer.js
+++ dummy/src/swisstopoLayer.js
@@ -1,10 +1,13 @@
-/* Dieser Code erstellt mithilfe von OpenLayers eine WMS-Kachelschicht von swisstopo */
+//---------------------------------------------------------------
+//  Dieser Code erstellt mithilfe von OpenLayers 
+// eine WMS-Kachelschicht von swisstopo. In diesem
+// Beispiel wird die Schicht "ch.swisstopo.swissimage" verwendet
+//--------------------------------------------------------------
 import TileLayer from "ol/layer/Tile"; 
 import { TileWMS } from "ol/source";
 
 export function SwisstopoLayer(extent) {
   return new TileLayer({
-    // Verwende direkt den importierten Namen TileLayer
     extent: extent,
     source: new TileWMS({
       // Basis-URL
